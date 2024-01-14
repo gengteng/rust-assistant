acf2d5e0495d72e13c419d189fc9f35aed4ef580
gengteng <me@gteng.org>
2024-01-14 20:39:33 +0800

    feat: add local

---
+++ crates/rust-assistant-local/src/main.rs
@@ -0,0 +1,8 @@
+use std::net::SocketAddr;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let listener = TcpListener::bind(SocketAddr::from(([0u8, 0, 0, 0], 3000))).await?;
+    Ok(axum::serve(listener, rust_assistant::router().into_make_service()).await?)
+}
--- crates/rust-assistant/src/lib.rs
+++ crates/rust-assistant/src/lib.rs
@@ -1,14 +1,55 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use axum::extract::{Path, Query};
+use axum::routing::get;
+use axum::{Json, Router};
+use serde::{Deserialize, Serialize};
+use std::sync::Arc;
+
+#[derive(Debug, Deserialize, Serialize, Hash, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub struct CrateVersion {
+    #[serde(rename = "crate")]
+    pub krate: Arc<str>,
+    pub version: Arc<str>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Debug, Deserialize, Serialize)]
+pub struct CrateVersionPath {
+    #[serde(flatten)]
+    pub crate_version: CrateVersion,
+    pub path: Arc<str>,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[derive(Debug, Deserialize, Serialize)]
+pub struct FileLineRange {
+    pub start: Option<u64>,
+    pub end: Option<u64>,
+}
+
+async fn read_ast(Path(path): Path<CrateVersionPath>) -> Json<CrateVersionPath> {
+    Json(path)
+}
+
+async fn read_file(
+    Path(path): Path<CrateVersionPath>,
+    Query(range): Query<FileLineRange>,
+) -> Json<(CrateVersionPath, FileLineRange)> {
+    Json((path, range))
+}
+
+async fn read_directory(Path(path): Path<CrateVersionPath>) -> Json<CrateVersionPath> {
+    Json(path)
+}
+
+async fn read_crate_directory(Path(path): Path<CrateVersion>) -> Json<CrateVersion> {
+    Json(path)
+}
+
+pub fn router() -> Router {
+    let directory_app = Router::new()
+        .route("/", get(read_crate_directory))
+        .route("/*path", get(read_directory));
+
+    Router::new()
+        .route("/api/ast/:crate/:version/*path", get(read_ast))
+        .route("/api/file/:crate/:version/*path", get(read_file))
+        .nest("/api/directory/:crate/:version", directory_app)
 }
