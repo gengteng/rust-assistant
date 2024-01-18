23ab213244e2527433748cf7b7d125bc8ab39b10
gengteng <me@gteng.org>
2024-01-18 20:29:36 +0800

    chore: update

--- crates/rust-assistant-local/src/main.rs
+++ crates/rust-assistant-local/src/main.rs
@@ -4,13 +4,11 @@ use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let (Some(username), Some(password)) = (
-        dotenv::var("API_USERNAME").ok(),
-        dotenv::var("API_PASSWORD").ok(),
-    ) else {
-        return Err(anyhow::anyhow!(
-            "'API_USERNAME' and 'API_PASSWORD' must be provided",
-        ));
+    let Some(username) = dotenv::var("API_USERNAME").ok() else {
+        return Err(anyhow::anyhow!("'API_USERNAME' must be provided",));
+    };
+    let Some(password) = dotenv::var("API_PASSWORD").ok() else {
+        return Err(anyhow::anyhow!("'API_PASSWORD' must be provided",));
     };
     let listener = TcpListener::bind(SocketAddr::from(([0u8, 0, 0, 0], 3000))).await?;
     Ok(axum::serve(
--- crates/rust-assistant-shuttle/src/main.rs
+++ crates/rust-assistant-shuttle/src/main.rs
@@ -5,12 +5,14 @@ use shuttle_runtime::CustomError;
 async fn main(
     #[shuttle_secrets::Secrets] secret_store: shuttle_secrets::SecretStore,
 ) -> shuttle_axum::ShuttleAxum {
-    let (Some(username), Some(password)) = (
-        secret_store.get("API_USERNAME"),
-        secret_store.get("API_PASSWORD"),
-    ) else {
+    let Some(username) = secret_store.get("API_USERNAME") else {
         return Err(shuttle_runtime::Error::Custom(CustomError::msg(
-            "'API_USERNAME' and 'API_PASSWORD' must be provided",
+            "'API_USERNAME' must be provided",
+        )));
+    };
+    let Some(password) = secret_store.get("API_PASSWORD") else {
+        return Err(shuttle_runtime::Error::Custom(CustomError::msg(
+            "'API_PASSWORD' must be provided",
         )));
     };
     Ok(rust_assistant::axum::router(AuthInfo::from((username, password))).into())
